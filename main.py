6c2f485981c9bb416c37a433739569c79df6d9c9
Facultad de Ing Sistemas e informatica <informatica@bga.upb>
2019-02-22 11:56:51 -0500

    modificación

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-#algoritmo que permita calcular el producto entre un número entero a
-#y un número entero número entero c dado,
-#también deberá calcular el doble del número a
+import math
+#Calcular el producto entre un número entero a y un número entero c
+#También deberá calcular el doble del número a
 
 na = int(input("Ingrese un número a: "))
-nc = int(input("Ingrese un número b: "))
+nc = int(input("Ingrese un número c: "))
 
 producto = na*nc
 print()
@@ -11,3 +11,19 @@ print ("La multiplicación entre a y b es: " + str(producto))
 print()
 doble_a = 2*na
 print ("El doble de a es: " + str(doble_a))
+print()
+
+#Calcular el cuadrado del numero entero b
+
+nb = int(input("Ingrese un numero b: "))
+potencia = nb*nb
+print(str(nb) + " al cuadrado es: " + str(potencia))
+print()
+
+#Y la raiz cuadrada del numero d
+
+#TO FIX
+
+#nd = int(input("Ingrese un numero d: "))
+#raiz = sqrt(nd)
+#print ("La raiz de d es: " + str(raiz))
