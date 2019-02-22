c623077a635b4a2bf1650c98ac20590bfdf08226
Facultad de Ing Sistemas e informatica <informatica@bga.upb>
2019-02-22 11:12:43 -0500

    Primer commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#algoritmo que permita calcular el producto entre un número entero a
+#y un número entero número entero c dado,
+#también deberá calcular el doble del número a
+
+na = int(input("Ingrese un número a: "))
+nc = int(input("Ingrese un número b: "))
+
+producto = na*nc
+print()
+print ("La multiplicación entre a y b es: " + str(producto))
+print()
+doble_a = 2*na
+print ("El doble de a es: " + str(doble_a))
